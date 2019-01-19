5cb0a535fd9576d835f87626544afeadfbbe6a09
Ruby Bantock <banruby@gmail.com>
2019-01-19 10:43:17 -0500

    Basic functionality in place.

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
 
-const myApp = {
+// the array that holds all the value input by the user
+const pokedex = {
 	data: []
 }
 
-
-// get array of pokemon from the kanto region
-// could be modified to be any region but is currently specific in the code
-const getRegionData = () => {
-	let pokePromise = $.ajax({
-		url : `https://pokeapi.co/api/v2/pokedex/kanto/`,
-		dataType : 'json',
-		method: 'GET'
-	}).then(function(data){
-		let pokedex = data;
-		console.log(pokedex)
-		
-	})
-}
-
-const validate = (guessedName, validName) => {
-	if (guessedName == validName) {
-		console.log("COMPARISON!")
-		// then run addImage here
-	}
-	// MAYBE ADD SOME ANIMATION ON THE INPUT?
-}
-
-getRegionData();
-
-
+// the function that gets data for each input from the API
 const getData = (inputPokemonFormatted) => {
 	let pokePromise = $.ajax({
 		url : `https://pokeapi.co/api/v2/pokemon/${inputPokemonFormatted}/`,
@@ -40,42 +16,29 @@ const getData = (inputPokemonFormatted) => {
 		// console.log(data.types[0].type.name);
 		// console.log(data.weight);
 		let pokeData = data;
-		myApp.data.push(pokeData.name);
+		pokedex.data.push(pokeData.name);
 		addImage(pokeData);
-		console.log(pokeData);
-		
-		
-		// if (pokeData.name === regionDex.name) {
-		// 	console.log("IT'S A MATCH");			
+		console.log(pokeData);			
 	});
 }
 
+// let lettersOnly = () => {
+
+// }
 
 
 
-const shouldFetchData = (name) => {
+let counter = 0;
 
-
-
-
-// 	const existingPokemon = myApp.data.find(function(pokemon){
-// 		if (name === pokemon.name){
-// 			return true
-// 		} else
-// 		return false
-// 	})
-// 	if (existingPokemon) {
-// 		return false
-// 	} else return true
-}		
 
 const addImage = (pokeData) => {
 	$(".pokemon-gallery").prepend(`
 		<div class="pokemon-container">
-			<img src="${pokeData.sprites.front_default}">
-			<p class="blue">${pokeData.name}!!!</p>
-			<p>${pokeData.id}</p>
-			<p>${pokeData.types[0].type.name}</p>
+			<img class="pokeImage" src="${pokeData.sprites.front_default}">
+			<div class="pokeTextContainer">
+				<p class="pokeNumber">${pokeData.id}</p>
+				<p class="pokeName">${pokeData.name}</p>
+			</div>
 		</div>`
 	);
 }
@@ -86,12 +49,19 @@ $("#pokeSubmit").submit(function(e){
 	e.preventDefault();
 	let inputPokemon = $("input[name=userGuess]").val();
 	let inputPokemonFormatted = inputPokemon.toLowerCase();
-	// if (shouldFetchData(inputPokemonFormatted)){
-		if (!myApp.data.includes(inputPokemonFormatted)){
+
+	let myRegEx = /[0-9]/
+	let stringTest = myRegEx.test(inputPokemonFormatted);
+	console.log(inputPokemon);
+	if (inputPokemon == "") {
+	} else if (!pokedex.data.includes(inputPokemonFormatted) && !stringTest){
 		getData(inputPokemonFormatted);
+		counter = counter + 1;
+		$("span.totalGuessed").replaceWith(`<span class="totalGuessed">${counter}</span>`)
 	}
+		
+})
 
 
 	//NEEDS AN IF STATEMENT TO RUN ONLY IF THERE ISN'T AN ERROR
 
-})
