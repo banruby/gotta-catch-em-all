f731498c24ab699c5fd1ed57be3f39d8847c5f60
Ruby Bantock <banruby@gmail.com>
2019-01-20 14:06:58 -0500

    Added responsive features.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-// the array that holds all the value input by the user
-const pokedex = {
-	data: []
-}
-let counter = 0; //counts successful inputs
 
-const $inputReset = () => {
-	$("#userGuess").val("");
-}
 $(function(){ //doc ready
+
+	// the array that holds all the value input by the user
+	const pokedex = {
+		data: []
+	}
+	let counter = 0; //counts successful inputs
+
+	const $inputReset = () => {
+		$("#userGuess").val("");
+	}
 
 	//changes overlay colour based on hover
 	$("li.bulbasaur").hover(function() {
@@ -19,7 +21,7 @@ $(function(){ //doc ready
 	})
 
 	$("li.pikachu").hover(function() {
-		$(".overlay-top").css("background-color", "#f6e652")
+		$(".overlay-top").css("background-color", "#f6e652") 
 	})
 
 	$("li.squirtle").hover(function() {
@@ -36,6 +38,7 @@ $(function(){ //doc ready
 		})
 	})
 
+	//sets colour scheme
 	$("li.bulbasaur").click(function(){
 		$("body").addClass("bulbasaur-1")
 		$("header").addClass("bulbasaur-2")
@@ -46,6 +49,7 @@ $(function(){ //doc ready
 		getData("bulbasaur");
 	})
 
+	//sets colour scheme
 	$("li.squirtle").click(function(){
 		$("body").addClass("squirtle-1")
 		$("header").addClass("squirtle-2")
@@ -56,6 +60,7 @@ $(function(){ //doc ready
 		getData("squirtle");
 	})
 
+	//sets colour scheme
 	$("li.pikachu").click(function(){
 		$("body").addClass("pikachu-1")
 		$("header").addClass("pikachu-2")
@@ -66,6 +71,7 @@ $(function(){ //doc ready
 		getData("pikachu");
 	})
 
+	//sets colour scheme
 	$("li.charmander").click(function(){
 		$("body").addClass("charmander-1")
 		$("header").addClass("charmander-2")
@@ -76,7 +82,7 @@ $(function(){ //doc ready
 		getData("charmander");
 	})
 
-// the function that gets data for each input from the API
+	//gets data from API
 	const getData = (inputPokemonFormatted) => {
 		let pokePromise = $.ajax({
 			url : `https://pokeapi.co/api/v2/pokemon/${inputPokemonFormatted}/`,
@@ -92,11 +98,12 @@ $(function(){ //doc ready
 		});
 	}
 
+	// counts valid entries
 	const counterUp = (increase) => {
 		counter = counter + increase;
 	}
 
-
+	// adds image block into grid
 	const $addImage = (pokeData) => {
 		$(".pokemon-gallery").prepend(`
 			<div class="pokemon-container">
@@ -111,18 +118,18 @@ $(function(){ //doc ready
 		$("span.totalGuessed").replaceWith(`<span class="totalGuessed">${counter}</span>`)
 	}
 
-//NEEDS DOC READY
-// on click submits pokemon name for the user
-$("#pokeSubmit").submit(function(e){
-	e.preventDefault();
-	let inputPokemon = $("input[name=userGuess]").val();
-	let inputPokemonFormatted = inputPokemon.toLowerCase();
-	let myRegEx = /[0-9]/
-	let stringTest = myRegEx.test(inputPokemonFormatted);
-	if (!pokedex.data.includes(inputPokemonFormatted) && !stringTest){
-		getData(inputPokemonFormatted);
-	}
-})
+
+	// on click submits pokemon name for the user
+	$("#pokeSubmit").submit(function(e){
+		e.preventDefault();
+		let inputPokemon = $("input[name=userGuess]").val();
+		let inputPokemonFormatted = inputPokemon.toLowerCase();
+		let myRegEx = /[0-9]/
+		let stringTest = myRegEx.test(inputPokemonFormatted);
+		if (!pokedex.data.includes(inputPokemonFormatted) && !stringTest){
+			getData(inputPokemonFormatted);
+		}
+	})
 
 }) // ends doc ready
 
